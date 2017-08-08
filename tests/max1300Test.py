03d91b94bd1fe018d7e2976002f233967e4678a3
root <root@beaglebone.localdomain>
2017-08-08 16:05:43 +0000

    changing thermistor code

--- tests/max1300Test.py
+++ tests/max1300Test.py
@@ -1,3 +1,6 @@
+import timeit
+
+
 
 
 
@@ -7,11 +10,38 @@ def calculateTempC(thermistorVolt, r2, sourceVoltage):
     #print("Thermistor current = ", thermistorCurent)
     thermistorR = thermistorVolt / thermistorCurent
     #print("Thermistor resistance = ", thermistorR)
-    thermistorR = 30000
+    
     print(thermistorR)
-    thermistorTemperatureC = 94974.5413538519 - 4696.967 * thermistorR + 129.826 * thermistorR**2 - 2.6186 * thermistorR**3 + 0.042544* thermistorR**4 - 0.00059762 * thermistorR**5 + 0.00000781 * thermistorR**6 - 9.53630e-8 * thermistorR**7 + 9.691e-10 * thermistorR**8 - 7.19177773e-12 * thermistorR**9 + 3.497936e-1 * thermistorR**10 - 9.821e-17 * thermistorR**11 + 1.2e-19 * thermistorR**12
-    #thermistorTemperatureC = -3e-20 * thermistorR ** 5 + 1e-17 * thermistorR ** 4 + 1e-12 * thermistorR ** 2 + thermistorR - 2  
+    findThermistorValue(thermistorR)
 
-    print(thermistorTemperatureC)
+
+
+
+tempList = [[94980, 0],[90410, 1],[86090, 2],[81990, 3],[78110, 4],[74440, 5],[70960, 6],[67660, 7],[64530, 8],[61560, 9],[58750, 10],[56070, 11],[53540, 12],[51130, 13],[48840, 14],[46670, 15],[44600, 16],[42640, 17],[40770, 18],[38990, 19],[37300, 20],[35700, 21],[34170, 22],[32710, 23],[31320, 24],[30000, 25],[28740, 26],[27540, 27],[26400, 28],[25310, 29],[24270, 30],[23280, 31],[22330, 32],[21430, 33],[20570, 34],[19740, 35],[18960, 36],[18210, 37],[17490, 38],[16800, 39],[16150, 40],[15520, 41],[14920, 42],[14350, 43],[13800, 44],[13280, 45],[12770, 46],[12290, 47],[11830, 48],[11390, 49],[10970, 50],[10570, 51],[10180, 52],[9807, 53],[9450, 54],[9109, 55],[8781, 56],[8467, 57],[8166, 58],[7876, 59],[7599, 60],[7332, 61],[7076, 62],[6830, 63],[6594, 64],[6367, 65],[6149, 66],[5940, 67],[5738, 68],[5545, 69],[5359, 70],[5180, 71],[5007, 72],[4842, 73],[4682, 74],[4529, 75],[4381, 76],[4239, 77],[4102, 78],[3970, 79],[3843, 80],[3720, 81],[3602, 82],[3489, 83],[3379, 84],[3273, 85],[3172, 86],[3073, 87],[2979, 88],[2887, 89],[2799, 90],[2714, 91],[2632, 92],[2552, 93],[2476, 94],[2402, 95],[2331, 96],[2262, 97],[2195, 98],[2131, 99],[2069, 100],[2009, 101],[1950, 102],[1894, 103],[1840, 104],[1788, 105],[1737, 106],[1688, 107],[1640, 108],[1594, 109],[1550, 110],[1507, 111],[1465, 112],[1425, 113],[1386, 114],[1348, 115],[1311, 116],[1276, 117],[1241, 118],[1208, 119],[1176, 120],[1145, 121],[1114, 122],[1085, 123],[1057, 124],[1029, 125],[1002, 126],[976.3, 127],[951.1, 128],[926.7, 129],[903, 130],[880, 131],[857.7, 132],[836.1, 133],[815, 134],[794.6, 135],[774.8, 136],[755.6, 137],[736.9, 138],[718.8, 139],[701.2, 140],[684.1, 141],[667.5, 142],[651.3, 143],[635.6, 144],[620.3, 145],[605.5, 146],[591.1, 147],[577.1, 148],[563.5, 149],[550.2, 150]]
+
+def findThermistorValue(thermRes):
+    upperValue = 95000
+    print("Starting search")
+    start_time = timeit.default_timer()
+    #for value in tempList:
+    for index in range(len(tempList)):
+        if(index < range):
+            value = tempList[index][0]
+            lowerValue = tempList[index + 1][0]
+            upperBound = (upperValue - value)/2 + value
+            lowerBound = (value - lowerValue)/ 2 + lowerValue
+            
+            print lowerValue, thermRes, upperBound
+        
+        # res2 = (res1 - value[0])/2 + value[0]
+        # res1 = value[0]
+        # print(thermRes, res2, value[0])
+        # if(thermRes <= res2) and (thermRes >= value[0]):
+        #     elapsed = timeit.default_timer() - start_time
+        #     print(value[1])
+        #     print("Found it in ", elapsed * 1000000, "micro seconds")
+        #     break
+
+
 
 calculateTempC(4.06, 5510, 5)
