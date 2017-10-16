98423918c418c88e78eb58bccaf5a70a14b3b4c0
Jack Walker <jackw2050@users.noreply.github.com>
2017-10-16 15:36:01 -0500

    Update DS1390U.py

--- DS1390U.py
+++ DS1390U.py
@@ -15,3 +15,42 @@ spi.bpw = 8;#Bits per word
 spi.msh=1000000
 
 
+secondsAddress 		    	  = 0x81
+minutesAddress 			      = 0x82
+hoursAddress			        = 0x83
+dayOfWeekAddress		      = 0x84
+dayOfMonthAddress		      = 0x85
+monthCenturAddressy	    	= 0x86
+yearAddressr				      = 0x87
+
+secondsReadAddress 			  = 0x01
+minutesReadAddress 			  = 0x02
+hoursReadAddress			    = 0x03
+dayOfWeekReadAddress		  = 0x04
+dayOfMonthReadAddress		  = 0x05
+monthCenturReadAddressy		= 0x06
+yearReadAddressr				  = 0x07
+
+
+controlAddress			      = 0x0D
+statusAddress			        = 0x0E
+trickleChargerAddress     = 0x0F
+
+agingOffsetAddress		    = 0x10
+temperatureMSBAddress	  ` = 0x11
+temperatureLSBAddress	    = 0x12
+
+
+
+seconds 		= 0x00
+minutes 		= 0x00
+hours			= 0x00
+dayOfWeek		= 00000XXX
+dayOfMonth		= 00NNXXXX
+monthCentury	= 0x00
+year			= 0x00
+control			= 00011000 #Enable 1Hz or 00?
+status			= 0x00
+agingOffset		= 0x00
+temperatureMSB	= 0x00
+temperatureLSB	= 0x00
