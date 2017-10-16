7c951bd775cdafee1264231c74a81c47ec5d4fda
Jack Walker <jackw2050@users.noreply.github.com>
2017-10-16 15:23:14 -0500

    Update DS1390U.py

--- DS1390U.py
+++ DS1390U.py
@@ -1,1 +1,17 @@
 
+from Adafruit_BBIO.SPI import SPI
+
+import time
+#https://github.com/adafruit/adafruit-beaglebone-io-python
+# Constants
+VREF = 4.36
+
+
+spi = SPI(0,0) 
+
+#spi.fd = -1;
+spi.mode = 3;
+spi.bpw = 8;#Bits per word
+spi.msh=1000000
+
+
