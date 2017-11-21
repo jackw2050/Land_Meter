3fcd85f4d7cba6563e67c61fafb599d10a397af7
Jack Walker <jackw2050@users.noreply.github.com>
2017-11-21 16:11:21 -0600

    Update pwm.py

--- pwm.py
+++ pwm.py
@@ -2,14 +2,19 @@ import Adafruit_BBIO.PWM as PWM
 import Adafruit_BBIO.GPIO as GPIO
 import time
 import datetime
-#PWM.start(channel, duty, freq=2000, polarity=0)
-# PWM.start("P9_14", 50)
-# PWM.set_frequency("P9_14", 10)
+import BBB_ADC
 
+#PWM.start(channel, duty, freq=2000, polarity=0)
+PWM.start("P8_46", 50)
+# PWM.set_frequency("P9_14", 10)
+debug = True
 SENSE_CLOCK = "P9_14"
-FORCE_PWM = "P8_19"
+FORCE_PWM = "P8_13"
+HEARTBEAT = "P8_46"
 SENSE_FREQUENCY = 10000
 FORCE_FREQUENCY = 125
+HEARTBEAT_FREQUENCY = 100
+current_duty_cycle = 0x00
 GPIO.setup("P8_11", GPIO.IN)
 
 # add pid code here or in main or in new file
@@ -18,8 +23,15 @@ GPIO.setup("P8_11", GPIO.IN)
 #optionally, you can set the frequency as well as the polarity from their defaults:
 # PWM.start("P9_14", 50, 1000, 1)
 def pwm_init():
-	PWM.start(SENSE_CLOCK, 50, SENSE_FREQUENCY, 1)
-	PWM.start(FORCE_PWM, 50, FORCE_FREQUENCY, 1)
+	global debug
+	if debug:
+		print "Initializing sense clock to ", SENSE_FREQUENCY, "Hz.   ","Duty cycle:  50.0%"
+		print "Initializing force clock to ", FORCE_FREQUENCY, "Hz.   ","Duty cycle:  50.0%"
+		print "Initializing heart beat clock to 1Hz.   ","Duty cycle:  50.0%"
+		
+	PWM.start(SENSE_CLOCK, 50, SENSE_FREQUENCY, 0)
+	PWM.start(FORCE_PWM, 50, FORCE_FREQUENCY, 0)
+	PWM.start(HEARTBEAT, 50.00, HEARTBEAT_FREQUENCY, 0)
 
 
 
@@ -31,21 +43,26 @@ def pwm_shutdown():
 
 
 def set_force_duty_cycle(duty_cycle):
+	global debug
 	PWM.set_duty_cycle(FORCE_PWM, duty_cycle)
+	if debug:
+		print "New Force PWM duty cycle: ", duty_cycle, "%"
 	
-PWM.start('P8_13', 50.00, 1, 0)
+# PWM.start('P8_13', 50.00, 1, 0)
 
-GPIO.add_event_detect("P8_11", GPIO.RISING) # look for RISING 
-
-
-count = 0
-while True:
+# GPIO.add_event_detect("P8_11", GPIO.RISING) # look for RISING 
+#pwm_init()
+# count = 0
+# while True:
 	
-	#time.sleep(.001)
+# 	#time.sleep(.001)
 
-	if GPIO.event_detected("P8_11"): 
-		count = count + 1
-		print "Do one second stuff!", datetime.datetime.now(), count
-		if count >= 60:
-			count = 0
-			print "Doing one minute stuff"
+# 	if GPIO.event_detected("P8_11"): 
+# 		count = count + 1
+# 		print "ADC data 1000 count", datetime.datetime.now(), count
+# 		BBB_ADC.getAdcVal()
+# 		print ""
+# 		if count >= 60:
+# 			count = 0
+# 			print "Doing one minute stuff"
+
