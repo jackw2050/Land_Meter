ec08617ebd30f40ba508850d9c5fed6e85943676
Jack Walker <jackw2050@users.noreply.github.com>
2018-01-17 14:39:35 -0600

    Update BBB_ADC.py

--- BBB_ADC.py
+++ BBB_ADC.py
@@ -2,214 +2,194 @@
 
 import Adafruit_BBIO.ADC as ADC
 
-class BBB_ADC:
 
 
-    AIN0 = "P9_39"  #+12V
-    AIN1 = "P9_40"  #Lid thermistor
-    AIN2 = "P9_37"  #BBB +5V
-    AIN3 = "P9_38"  #N/C
-    AIN4 = "P9_33"  #+3.3V
-    AIN5 = "P9_36"  # Battery thermistor   
-    AIN6 = "P9_35"  # +5V
-    
-    
-    adc_average_count = 100
-    
-    ain0_divider = 0.090939
-    ain1_divider = 1.0
-    ain2_divider = 0.33402
-    ain3_divider = 1.0
-    ain4_divider = 0.13069
-    ain5_divider = 1.0
-    ain6_divider = 0.33307
+
+AIN0 = "P9_39"  #+12V
+AIN1 = "P9_40"  #Lid thermistor
+AIN2 = "P9_37"  #BBB +5V
+AIN3 = "P9_38"  #N/C
+AIN4 = "P9_33"  #+3.3V
+AIN5 = "P9_36"  # Battery thermistor   
+AIN6 = "P9_35"  # +5V
 
 
-    #These values should reside in file and loaded at boot
-    ain0_offset = 0.0
-    ain1_offset = 0.0
-    ain2_offset = 0.0
-    ain3_offset = 0.0
-    ain4_offset = 0.0
-    ain5_offset = 0.0
-    ain6_offset = 0.0
-    ain7_offset = 0.0
-    
-    
-    adc_offset = 0.004  #This value should reside in file
+adc_average_count = 100
+
+ain0_divider = 0.090939
+ain1_divider = 1.0
+ain2_divider = 0.33402
+ain3_divider = 1.0
+ain4_divider = 0.13069
+ain5_divider = 1.0
+ain6_divider = 0.33307
 
 
-    def __init__(self):
-        self.ADC.setup()
+#These values should reside in file and loaded at boot
+ain0_offset = 0.0
+ain1_offset = 0.0
+ain2_offset = 0.0
+ain3_offset = 0.0
+ain4_offset = 0.0
+ain5_offset = 0.0
+ain6_offset = 0.0
+ain7_offset = 0.0
+
+
+adc_offset = 0.004  #This value should reside in file
+
+ADC.setup()
 
 
 
 
-    def updateAinDivider(self, ainChan, offset):
-        if(ainChan == 0):
-            BBB_ADC.ain0_divider = offset
-            return 0
-        elif(ainChan == 1):
-            BBB_ADC.ain1_divider = offset
-            return 0
-        elif(ainChan == 2):
-            BBB_ADC.ain2_divider = offset
-            return 0
-        elif(ainChan == 3):
-            BBB_ADC.ain3_divider = offset
-            return 0
-        elif(ainChan == 4):
-            BBB_ADC.ain4_divider = offset
-            return 0
-        elif(ainChan == 5):
-            BBB_ADC.ain5_divider = offset
-            return 0
-        elif(ainChan == 6):
-            BBB_ADC.ain6_divider = offset
-            return 0
-        elif(ainChan == 7):
-            BBB_ADC.ain7_divider = offset 
-            return 0
-        else:   
-            print "ADC channel not defined.  divider value not updated"
-            return 1
-            
-    def updateAinOffset(self, ainChan, offset):
-        if(ainChan == 0):
-            BBB_ADC.ain0_offset = offset
-            return 0
-        elif(ainChan == 1):
-            BBB_ADC.ain1_offset = offset
-            return 0
-        elif(ainChan == 2):
-            BBB_ADC.ain2_offset = offset
-            return 0
-        elif(ainChan == 3):
-            BBB_ADC.ain3_offset = offset
-            return 0
-        elif(ainChan == 4):
-            BBB_ADC.ain4_offset = offset
-            return 0
-        elif(ainChan == 5):
-            BBB_ADC.ain5_offset = offset
-            return 0
-        elif(ainChan == 6):
-            BBB_ADC.ain6_offset = offset
-            return 0
-        elif(ainChan == 7):
-            BBB_ADC.ain7_offset = offset 
-            return 0
-        else:   
-            print "ADC channel not defined.  Offset not updated"
-            return 1
-    
-    def calcAdcValue(self, raw, divider, offset):
-        adjustedADCvalue = raw * divider + offset
-        return adjustedADCvalue
+def updateAinDivider(ainChan, offset):
+    if(ainChan == 0):
+        ain0_divider = offset
+        return 0
+    elif(ainChan == 1):
+        ain1_divider = offset
+        return 0
+    elif(ainChan == 2):
+        ain2_divider = offset
+        return 0
+    elif(ainChan == 3):
+        ain3_divider = offset
+        return 0
+    elif(ainChan == 4):
+        ain4_divider = offset
+        return 0
+    elif(ainChan == 5):
+        ain5_divider = offset
+        return 0
+    elif(ainChan == 6):
+        ain6_divider = offset
+        return 0
+    elif(ainChan == 7):
+        ain7_divider = offset 
+        return 0
+    else:   
+        print "ADC channel not defined.  divider value not updated"
+        return 1
+        
+def updateAinOffset(ainChan, offset):
+    if(ainChan == 0):
+        ain0_offset = offset
+        return 0
+    elif(ainChan == 1):
+        ain1_offset = offset
+        return 0
+    elif(ainChan == 2):
+        ain2_offset = offset
+        return 0
+    elif(ainChan == 3):
+        ain3_offset = offset
+        return 0
+    elif(ainChan == 4):
+        ain4_offset = offset
+        return 0
+    elif(ainChan == 5):
+        ain5_offset = offset
+        return 0
+    elif(ainChan == 6):
+        ain6_offset = offset
+        return 0
+    elif(ainChan == 7):
+        ain7_offset = offset 
+        return 0
+    else:   
+        print "ADC channel not defined.  Offset not updated"
+        return 1
 
-    def read_adc(self, adc_chan, loop_count, divider, offset):
-        chan = "P9_38"
-        if (adc_chan == "zhSys"):
-            chan = "P9_39"  #ZH
-            # chan = "ain0 etc."
-            # divider = BBB_ADC.ain0_divider
-            # offset = BBB_ADC.ain0_offset
-        elif (adc_chan == "lidThermistorVolt"):
-            chan = "P9_40"  #Lid thermistor
-            # chan = "ain1 etc."
-            # divider = BBB_ADC.ain1_divider
-            # offset = BBB_ADC.ain1_offset
-        elif (adc_chan == "p12vSys"):
-            chan = "P9_37"  #+12V
-            # chan = "ain2 etc."
-            # divider = BBB_ADC.ain2_divider
-            # offset = BBB_ADC.ain2_offset
-        elif (adc_chan == "p5vSys"):
-            chan = "P9_38"  #+5V
-            # chan = "ain3 etc."
-            # divider = BBB_ADC.ain3_divider
-            # offset = BBB_ADC.ain3_offset
-        elif (adc_chan == "p3p3vSys"):
-            chan = "P9_33"  #+3.3V
-            # chan = "ain4 etc."
-            # divider = BBB_ADC.ain4_divider
-            # offset = BBB_ADC.ain4_offset
-        elif (adc_chan == "battThermistorVolt"):
-            chan = "P9_36"  # Battery thermistor   
-            # chan = "ain5 etc."
-            # divider = BBB_ADC.ain5_divider
-            # offset = BBB_ADC.ain5_offset
-        elif (adc_chan == "battVolt"):
-            chan = "P9_35"  # Battery voltage
-            # chan = "ain6 etc."
-            # divider = BBB_ADC.ain6_divider
-            # offset = BBB_ADC.ain6_offset
-    
-        adcRange = 1.8
-        adc_value = 0.0
-    
-        for num in range (0, loop_count):
-            adc_value += ADC.read(chan)
-            #print adc_value
-            #print ADC.read(chan)
-    
-        adc_value /= loop_count
-        adc_value *= adcRange
-        adc_value = calcAdcValue(adc_value, divider, offset)
-        return adc_value
+def calcAdcValue(self, raw, divider, offset):
+    adjustedADCvalue = raw * divider + offset
+    return adjustedADCvalue
 
-    def checkLimits(self, chan, adcValue):
-            chan = "P9_38"
-            target = 5.0
-            lowLimit = 4.9
-            hiLimit = 5.1
+def read_adc(adc_chan, loop_count, divider, offset):
+    chan = "P9_38"
 
-        if (adc_chan == "zhSys"):
-            chan = "P9_39"  #ZH
-            target = 5.0
-            lowLimit = 4.9
-            hiLimit = 5.1
-        elif (adc_chan == "lidThermistorVolt"):
-            chan = "P9_40"  #Lid thermistor
-            target = 5.0
-            lowLimit = 4.9
-            hiLimit = 5.1
-        elif (adc_chan == "p12vSys"):
-            chan = "P9_37"  #+12V
-            target = 5.0
-            lowLimit = 4.9
-            hiLimit = 5.1
-        elif (adc_chan == "p5vSys"):
-            chan = "P9_38"  #+5V
-            target = 5.0
-            lowLimit = 4.9
-            hiLimit = 5.1
-        elif (adc_chan == "p3p3vSys"):
-            chan = "P9_33"  #+3.3V
-            target = 5.0
-            lowLimit = 4.9
-            hiLimit = 5.1
-        elif (adc_chan == "battThermistorVolt"):
-            chan = "P9_36"  # Battery thermistor   
-            target = 5.0
-            lowLimit = 4.9
-            hiLimit = 5.1
-        elif (adc_chan == "battVolt"):
-            chan = "P9_35"  # Battery voltage
-            target = 5.0
-            lowLimit = 4.9
-            hiLimit = 5.1
+    if (adc_chan == "lidThermistorVolt"):
+        chan = "P9_40"  #Lid thermistor
+        # chan = "ain1 etc."
+        # divider = ain1_divider
+        # offset = ain1_offset
+    elif (adc_chan == "p12vSys"):
+        chan = "P9_37"  #+12V
+        # chan = "ain2 etc."
+        # divider = ain2_divider
+        # offset = ain2_offset
+    elif (adc_chan == "p5vSys"):
+        chan = "P9_38"  #+5V
+        # chan = "ain3 etc."
+        # divider = ain3_divider
+        # offset = ain3_offset
+    elif (adc_chan == "p3p3vSys"):
+        chan = "P9_33"  #+3.3V
+        # chan = "ain4 etc."
+        # divider = ain4_divider
+        # offset = ain4_offset
+    elif (adc_chan == "battThermistorVolt"):
+        chan = "P9_36"  # Battery thermistor   
+        # chan = "ain5 etc."
+        # divider = ain5_divider
+        # offset = ain5_offset
 
-        if (adcValue < lowLimit or adcValue > hiLimit):
-            return -99
-        else:
-            return 1
+    adcRange = 1.8
+    adc_value = 0.0
+
+    for num in range (0, loop_count):
+        adc_value += ADC.read(chan)
+        #print adc_value
+        #print ADC.read(chan)
+
+    adc_value /= loop_count
+    adc_value *= adcRange
+    adc_value = calcAdcValue(adc_value, divider, offset)
+    return adc_value
+
+def checkLimits(chan, adcValue):
+    chan = "P9_38"
+    target = 5.0
+    lowLimit = 4.9
+    hiLimit = 5.1
+
+    if (adc_chan == "lidThermistorVolt"):
+        chan = "P9_40"  #Lid thermistor
+        target = 5.0
+        lowLimit = 4.9
+        hiLimit = 5.1
+    elif (adc_chan == "p12vSys"):
+        chan = "P9_37"  #+12V
+        target = 5.0
+        lowLimit = 4.9
+        hiLimit = 5.1
+    elif (adc_chan == "p5vSys"):
+        chan = "P9_38"  #+5V
+        target = 5.0
+        lowLimit = 4.9
+        hiLimit = 5.1
+    elif (adc_chan == "p3p3vSys"):
+        chan = "P9_33"  #+3.3V
+        target = 5.0
+        lowLimit = 4.9
+        hiLimit = 5.1
+    elif (adc_chan == "battThermistorVolt"):
+        chan = "P9_36"  # Battery thermistor   
+        target = 5.0
+        lowLimit = 4.9
+        hiLimit = 5.1
+
+
+    if (adcValue < lowLimit or adcValue > hiLimit):
+        return -99
+    else:
+        return 1
                     
 
 
 # print round(read_adc("p12vSys", 10, 11.01, 0), 3)
-# print round(read_adc("p5vSys", 10, 6.05, 0), 3)
-# print round(read_adc("p3p3vSys", 10, 2.9568, 0), 3)
+print round(read_adc("p5vSys", 10, 6.05, 0), 3)
+print round(read_adc("p3p3vSys", 10, 2.9568, 0), 3)
 
 
 # print ADC.read("P9_37") * 1.8
@@ -219,12 +199,12 @@ class BBB_ADC:
 
 #zh              = 1.8 * read_adc(AIN0, adc_average_count, 1 , 0)
 
-# lid_thermistor  = 1.8 * read_adc(AIN1, adc_average_count) * ain1_divider + adc_offset
-# p12v            = 1.8 * read_adc(AIN2, adc_average_count) * ain2_divider + adc_offset
-# p5v             = 1.8 * read_adc(AIN3, adc_average_count) * ain3_divider + adc_offset
-# p3p3v           = 1.8 * read_adc(AIN4, adc_average_count) * ain4_divider + adc_offset
-# batt_thermistor = 1.8 * read_adc(AIN5, adc_average_count) * ain5_divider + adc_offset
-# batt_v          = 1.8 * read_adc(AIN6, adc_average_count) * ain6_divider + adc_offset
+# lid_thermistor  = 1.8 * read_adc(AIN1, adc_average_count , ain1_divider , adc_offset)
+# p12v            = 1.8 * read_adc(AIN2, adc_average_count , ain2_divider , adc_offset)
+# p5v             = 1.8 * read_adc(AIN3, adc_average_count , ain3_divider , adc_offset)
+# p3p3v           = 1.8 * read_adc(AIN4, adc_average_count , ain4_divider , adc_offset)
+# batt_thermistor = 1.8 * read_adc(AIN5, adc_average_count , ain5_divider , adc_offset)
+# batt_v          = 1.8 * read_adc(AIN6, adc_average_count , ain6_divider , adc_offset)
 
 
 
