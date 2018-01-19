54b87e88a9dfc7f2d89d638ecd3c12d228458ba0
Jack Walker <jackw2050@users.noreply.github.com>
2018-01-19 15:02:52 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,4 +5,42 @@ import Levels
 #file_ops.create_basic_cal_file()
 #file_ops.read_cal_file()
 
-print Levels.get_level_in_arc_sec(65000)
+# print Levels.get_level_in_arc_sec(65000)
+
+
+import zlsGpio as ZLS_GPIO
+import heater
+
+Arrestment = heater('Arrestment', 54, 55, False,"P8_5", 0.0, 0.0)
+
+
+
+# ZLS_GPIO.zlsInit()
+
+# import file_ops
+# import Levels
+
+# #file_ops.create_log_entry("test")
+# #file_ops.create_basic_cal_file()
+# #file_ops.read_cal_file()
+
+# print Levels.get_level_in_arc_sec(65000)
+
+
+# from Adafruit_I2C import Adafruit_I2C
+ 
+# i2c = Adafruit_I2C(0x77)
+
+# P9_19: I2C2, SCL
+# P9_20: I2C2, SDA
+
+# write8(self, 0x01, 0xFA)
+
+
+# systemPower(0, debug)
+	   	
+
+# setThermistorMux(0,0,0,debug)
+# # # setFBMux(1,1,debug)
+# setHeater(1, 1, 0, 0, debug)
+# setMux(1,0,0,debug)
